569a859fe5c1e1f39ad01973c52c971191d41cb5
Paviv1105Appu <120457675+Paviv1105Appu@users.noreply.github.com>
2023-02-28 09:43:53 +0530

    Add files via upload

---
+++ ppp.java
@@ -0,0 +1,37 @@
+package suresh;
+
+//import java.util.List;
+//import java.util.concurrent.TimeUnit;
+
+/*
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ppp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\text\\Downloads\\selenium-java-4.8.0\\chrome\\Chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	    driver.manage().window().maximize();
+	    String url = "https://rahulshettyacademy.com/locatorspractice";
+	    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	     // driver.navigate().to("https://rahulshettyacademy.com/locatorspractice/");
+	   driver.get(url);
+	   WebElement web = driver.findElement(By.id("inputUsername"));
+	   if (web.isDisplayed()==true) {
+		   web.sendKeys("pavi");
+	   }
+	   List<WebElement> links = driver.findElements(By.tagName("a"));
+	   System.out.println(links.size());
+	   for(int i=0;i<links.size();i++) {
+		   System.out.println(links.get(i).getText());
+	   }
+
+	}
+
+}
+
+*/
---
+++ rightclick.java
@@ -0,0 +1,25 @@
+package suresh;
+
+/*
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class rightclick {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\text\\Downloads\\selenium-java-4.8.0\\chrome\\Chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	    driver.manage().window().maximize();
+	    String url = "https://rahulshettyacademy.com/locatorspractice";
+	    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	     // driver.navigate().to("https://rahulshettyacademy.com/locatorspractice/");
+	   driver.get(url);
+
+	}
+
+}
+
+*/
